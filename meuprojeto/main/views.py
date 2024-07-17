d6f15317bcbec3bf6e3e58a0e8e92dcb54dcbf9a
Marcelo Biotto <marcelocardosoconsultoria@gmail.com>
2024-07-17 20:36:41 -0300

    wip demais rotas flask

--- meuprojeto/main/views.py
+++ meuprojeto/main/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from main.bd_config import conecta_no_banco_de_dados
 from .forms import ContatoForm
-
+from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
     return render(request, 'Guia/index.html')
@@ -52,3 +53,43 @@ def contato(request):
         # Renderizar formulário vazio
         form = ContatoForm()
         return render(request, 'contato.html', {'form': form})
+    
+    
+    
+# @app.route('/login', methods=['GET', 'POST'])
+def login(request):
+    if request.method == 'POST':
+        template = loader.get_template('login.html')
+    #     form = ContatoForm(request.POST)
+    #     nome = request.form.get('nome')
+    #     email = request.form.get('email')
+    #     senha = request.form.get('senha')
+
+            
+    #     if not nome or not email or not senha:
+    #         return redirect(url_for('pagina_login'))
+
+    #     bd = conecta_no_banco_de_dados()
+    #     if bd is None:
+            
+    #      return redirect(url_for('pagina_login'))
+
+    #     cursor = bd.cursor()
+    #     try:
+    #         cursor.execute("SELECT * FROM contatos WHERE nome=%s AND email=%s AND senha=%s", (nome, email, senha))
+    #         user = cursor.fetchone()
+    #         if user:
+    #             session['usuario_id'] = user[0]
+    #             session['usuario_nome'] = user[1]
+    #             flash("Login realizado com sucesso.")
+    #             return redirect(url_for('index'))
+    #         else:
+    #             flash("Usuário ou senha incorretos.")
+    #     except mysql.connector.Error as err:
+    #         flash("Erro ao realizar login: {}".format(err))
+    #     finally:
+    #         cursor.close()
+    #         bd.close()
+
+    return HttpResponse(template.render(request))
+    # return render(request, 'login.html')
