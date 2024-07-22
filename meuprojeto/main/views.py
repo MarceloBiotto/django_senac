e2b2cbe2aaf081ea12b2ee243f3056f5102b9f78
Marcelo Biotto <marcelocardosoconsultoria@gmail.com>
2024-07-22 20:01:39 -0300

    wip cadastrar

--- meuprojeto/main/views.py
+++ meuprojeto/main/views.py
@@ -103,44 +103,77 @@ def login(request):
     
 
 def cadastrar(request):
-    nome = request.form.get('nome')
-    email = request.form.get('email')
-    senha = request.form.get('senha')
+     if request.method == 'POST':
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            try:
+                # Estabelecer conexão com o banco de dados
+                bd = conecta_no_banco_de_dados()
 
+                # Preparar consulta SQL e valores
+                nome = request.form.get('nome')
+                email = request.form.get('email')
+                senha = request.form.get('senha')
+                values = (nome, email,senha)
+
+                # Executar consulta SQL e confirmar alterações
+                cursor = bd.cursor()
+                cursor.execute(sql, values)
+                bd.commit()
+            except Exception as err:
+                # Manipular erros de banco de dados
+                print(f"Erro ao salvar dados no banco de dados: {err}")
+                mensagem_erro = "Ocorreu um erro ao processar o seu contato. Tente novamente mais tarde."
+                return render(request, 'erro.html', mensagem_erro=mensagem_erro), 500
+
+            finally:
+                # Fechar conexão com o banco de dados se estiver aberta
+                if bd is not None:
+                    bd.close()
     # Validação
-    if not nome:
-        print('O nome é obrigatório.')
-        return render('cadastrar.html')
-    if not email:
-        print('O e-mail é obrigatório.')
-        return render('cadastrar.html')
-    if not senha:
-        print('A senha é obrigatória.')
-        return render('cadastrar.html')
+        if not nome:
+            print('O nome é obrigatório.')
+            return render('cadastrar.html')
+        if not email:
+            print('O e-mail é obrigatório.')
+            return render('cadastrar.html')
+        if not senha:
+            print('A senha é obrigatória.')
+            return render('cadastrar.html')
 
-    bd = conecta_no_banco_de_dados()
-    cursor = bd.cursor()
-    cursor.execute("""
-        SELECT COUNT(*)
-        FROM contatos
-        WHERE email = %s;
-    """, (email,))
-    existe = cursor.fetchone()[0]
-    cursor.close()
-    bd.close()
+        bd = conecta_no_banco_de_dados()
+        cursor = bd.cursor()
+        cursor.execute("""
+            SELECT COUNT(*)
+            FROM usuarios
+            WHERE email = %s;
+            """, (email,))
+        existe = cursor.fetchone()[0]
+        cursor.close()
+        bd.close()
 
-    if existe > 0:
-        print('Email já cadastrado')
-        return render('cadastrar.html')
-    else:
-        try:
-            bd = conecta_no_banco_de_dados()
-            cursor = bd.cursor()
-            sql = 'INSERT INTO contatos (nome, email, senha) VALUES (%s, %s, %s)'
-            values = (nome, email, senha)
-            cursor.execute(sql, values)
-            bd.commit()
-            cursor.close()
-            return redirect('login')
-        except mysql.connector.Error as e:
-            return render('cadastrar.html', error=str(e))
+        if existe > 0:
+            print('Email já cadastrado')
+            return render('cadastrar.html')
+        else:
+            try:
+                bd = conecta_no_banco_de_dados()
+                cursor = bd.cursor()
+                sql = 'INSERT INTO usuarios (nome, email, senha) VALUES (%s, %s, %s)'
+                values = (nome, email, senha)
+                cursor.execute(sql, values)
+                bd.commit()
+                cursor.close()
+                form = ContatoForm()
+                return render(request, 'Login/login.html', {'form': form})
+            except Exception as err:
+                # Manipular erros de banco de dados
+                print(f"Erro ao salvar dados no banco de dados: {err}")
+                mensagem_erro = "Ocorreu um erro ao processar o seu contato. Tente novamente mais tarde."
+                return render(request, 'erro.html', mensagem_erro=mensagem_erro), 500
+
+            finally:
+                # Fechar conexão com o banco de dados se estiver aberta
+                if bd is not None:
+                    bd.close()
+            
